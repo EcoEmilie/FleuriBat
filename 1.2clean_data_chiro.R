1fb2f07c5211c4ea003a89c99792bdfce26f3a4e
Ecoemilie <pen.emilie@hotmail.fr>
2023-04-25 13:58:12 +0200

    Update 1.2clean_data_chiro.R
    modification des chemins

--- 1.2clean_data_chiro.R
+++ 1.2clean_data_chiro.R
@@ -1,5 +1,5 @@
 # Titre : 1.2clean_data_chiro
-# But : Filtrer les resultats de Tadarida, on ne veut garder que les donnees chiropteres
+# But : Filtrer les resultats de Tadarida, on ne veut garder que les donnees chiropteres + remettre les dates correctements +
 # Auteur : Emilie
 # Date : 21/04/2023
 
@@ -7,27 +7,25 @@
 # Library -----------------------------------------------------------------
 
 library(tidyverse)
+library(lubridate)
 
 
 # Chargement donnees ------------------------------------------------------
+Folderpath = paste("~/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees")
 
-setwd("~/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees/2.Donnees_intermediaire/Chiroptères")
+data_brute = readRDS(file = file.path(Folderpath,"2.Donnees_intermediaire","export_fusion.rds"))
 
-data_brute = readRDS("export_fusion.rds")
-
-# Nettoyage  --------------------------------------------------------------
-
-## Liste esp??ce
-
-setwd("/Users/emihui/Library/Mobile Documents/com~apple~CloudDocs/FAC/Master/M2/Stage/Stage_ESE-OFB/Statistiques/_Donnees")
-list_esp = read.csv("SpeciesList.csv", sep = ";") %>% 
+list_esp = read.csv(file = file.path(Folderpath,"1.Donnees_sources","Chiroptères","Informations","SpeciesList.csv"), sep = ";") %>% 
   filter(Group == "bat")
 
-## On ne garde que les chauves souris 
-data_total = a_192021 %>% 
-  unite(point_year_pass,Carre_Point_vigiechiro,year,Num_passag, sep = "_", remove = FALSE) %>% 
+# Filtrage ----------------------------------------------------------------
+data_chiro = data_brute %>% 
+  unite(carre_year_pass,Carre_Point_vigiechiro,year,Num_passag, sep = "_", remove = FALSE) %>% 
   mutate(date=ymd(date)) %>% 
   mutate(year = factor(year)) %>% 
   filter(tadarida_taxon %in% list_esp$Esp) %>% #on garde que les CS
   mutate(tadarida_taxon= factor(tadarida_taxon))
 
+
+## Fichier RDS -------------------------------------------------------------
+saveRDS(data_chiro, file = file.path(Folderpath,"2.Donnees_intermediaire", "data_chiro.rds"))

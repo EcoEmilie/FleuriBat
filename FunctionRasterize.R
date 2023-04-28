350bab4eaf6498763ae9ac76dbf8f0289379d461
Ecoemilie <pen.emilie@hotmail.fr>
2023-04-28 11:33:46 +0200

    Update FunctionRasterize.R
    transformation en facteur

--- FunctionRasterize.R
+++ FunctionRasterize.R
@@ -16,10 +16,11 @@ library(rgdal)
 RasterizeFunction = function( shp, res, field){
   Zproj<-"+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +units=m +no_defs"
   ZONE <- readOGR(shp)
+  ZONE$field = as.factor(ZONE$field)
   ZRASTER <- raster(ext=extent( ZONE ),res=res,crs=Zproj, vals=NA)
   R <- rasterize(ZONE, ZRASTER, field=ZONE$field)
-  return(R)
 }
+
 # x = file.path(Folderpath, FolderCarto, "data_naturel.gpkg")
 # RasterizeFunction(x, 10, Nature)
 # 

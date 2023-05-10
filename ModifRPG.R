6e45efdff19c1836c7c1c783d4ee6339af6f8b6e
Ecoemilie <pen.emilie@hotmail.fr>
2023-05-10 14:54:42 +0200

    Create ModifRPG.R
    Chargement des RPG des différentes régions + modification des RPG pour reduire leur taille en faisant un buffer de 4000m autour des sites

---
+++ ModifRPG.R
@@ -0,0 +1,96 @@
+# Titre : ModifRPG 
+# But : Charger les RPG selon les années et les régions, les fusionner et faire une buffer de 4000m autours des sites pour alléger 
+# Auteur : Emilie PEN
+# Date : 10/04/2023
+
+
+# Library  ----------------------------------------------------------------
+
+library(sf)
+library(tidyverse)
+
+
+# Chemin  -----------------------------------------------------------------
+
+FolderDonnees = paste("/Users/emihui/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees/1.Donnees_sources/Cartographie/RPG")
+RPG2021 = "RPG_2021"
+RPG2020 = "RPG_2020"
+RPG2019 = "RPG_2019"
+IDF = "IDF"
+CVL = "Centre_Val_de_Loire"
+N = "Normandie"
+
+FolderFinal = paste("/Users/emihui/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees/2.Donnees_intermediaire")
+  
+FolderSite = paste("/Users/emihui/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees/2.Donnees_intermediaire")
+
+#Site
+data_site_sf = readRDS(file.path(FolderSite,"data_site.rds")) %>% 
+  st_as_sf(coords = c("X","Y"), crs = 4326) %>% #au départ en WGS 84
+  st_transform(2154) 
+
+# Chargement des données 2021 ---------------------------------------------
+
+data_RPG_2021_IDF = st_read(file.path(FolderDonnees,RPG2021,"IDF/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2021_CVL = st_read(file.path(FolderDonnees,RPG2021,"Centre_Val_de_Loire/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2021_N= st_read(file.path(FolderDonnees,RPG2021,"Normandie/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2021 = st_as_sf(rbind(data_RPG_2021_CVL,data_RPG_2021_IDF, data_RPG_2021_N))
+
+
+data_site_sf_2021 =  data_site_sf %>% 
+  filter(year==2021) %>% 
+  select(geometry)
+
+data_RPG_2021_B = st_intersection(data_RPG_2021, st_buffer(data_site_sf_2021, dist = 4000)) 
+
+st_write(data_RPG_2021_B,file.path(FolderDonnees,"donnees_RPG_2021.gpkg"), driver = "GPKG", append = FALSE)
+saveRDS(data_RPG_2021_B,file.path(FolderDonnees,"donnees_RPG_2021.rds"))
+
+# Chargement données RPG 2020 --------------------------------------------------
+
+data_RPG_2020_IDF = st_read(file.path(FolderDonnees,RPG2020,"Ile_de_France/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2020_CVL = st_read(file.path(FolderDonnees,RPG2020,"Centre_Val_de_Loire/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2020_N= st_read(file.path(FolderDonnees,RPG2020,"Normandie/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2020 = st_as_sf(rbind(data_RPG_2020_CVL,data_RPG_2020_IDF, data_RPG_2020_N))
+
+data_site_sf_2020 = data_site_sf %>% 
+  filter(year==2020) %>% 
+  select(geometry)
+
+data_RPG_2020_B = st_intersection(data_RPG_2020, st_buffer(data_site_sf_2020, dist = 4000)) 
+
+st_write(data_RPG_2020_B,file.path(FolderDonnees,"donnees_RPG_2020.gpkg"), driver = "GPKG", append = FALSE)
+saveRDS(data_RPG_2020_B,file.path(FolderDonnees,"donnees_RPG_2020.rds"))
+
+# Chargement données RPG 2019 --------------------------------------------------
+
+data_RPG_2019_IDF = st_read(file.path(FolderDonnees,RPG2019,"IDF/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2019_CVL = st_read(file.path(FolderDonnees,RPG2019,"Centre_Val_de_Loire/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2019_N= st_read(file.path(FolderDonnees,RPG2019,"Normandie/PARCELLES_GRAPHIQUES.shp")) %>% 
+  st_transform(2154)
+
+data_RPG_2019 = st_as_sf(rbind(data_RPG_2019_CVL,data_RPG_2019_IDF, data_RPG_2019_N))
+
+data_site_sf_2019 = data_site_sf %>% 
+  filter(year==2019) %>% 
+  select(geometry)
+
+data_RPG_2019_B = st_intersection(data_RPG_2019, st_buffer(data_site_sf_2019, dist = 4000)) 
+st_write(data_RPG_2019_B,file.path(FolderDonnees,"donnees_RPG_2019.gpkg"), driver = "GPKG", append = FALSE)
+saveRDS(data_RPG_2019_B,file.path(FolderDonnees,"donnees_RPG_2019.rds"))

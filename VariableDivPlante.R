8105eb416bf7128eb41dc172dbdb95877a251051
Ecoemilie <pen.emilie@hotmail.fr>
2023-05-16 14:13:55 +0200

    Create VariableDivPlante.R
    création de l'indice de shannon de la div des plantes

---
+++ VariableDivPlante.R
@@ -0,0 +1,51 @@
+# Titre : VariableDivPlante
+# But : Calcule de la diversité des plantes dans les bandes 
+# Auteur : Emilie
+# Date : 12.05.2023
+
+rm(list=ls())
+
+# Library -----------------------------------------------------------------
+
+library(tidyverse)
+library(stringr)
+
+
+# Donnée ------------------------------------------------------------------
+
+FolderDonnees = paste("/Users/emihui/Documents sur ordi/Master/Stage_M2_ESE_OFB/R/Repertoire_donnees")
+FolderSources = "1.Donnees_sources"
+Bande = "Bande"
+FolderInter= "2.Donnees_intermediaire"
+FolderSortie = "3.Sorties"  
+
+Releve_bota = read.csv2(file.path(FolderDonnees,FolderSources, Bande, "Releve_bota_bandes.csv"),  sep = ";", dec = ",") 
+
+Releve_bota_modif = Releve_bota %>% 
+  mutate(recouvrement = str_replace(recouvrement,",", ".")) %>% 
+  mutate(recouvrement = as.numeric(recouvrement),
+         bande = as.factor(bande),
+         annee = as.factor(annee),
+         seme = as.factor(seme))
+
+Releve_NA = Releve_bota %>% 
+  filter(recouvrement == "NA")
+
+Richesse_spe = Releve_bota %>% 
+  group_by(annee,bande) %>% 
+  summary()
+
+Semee_spont = Releve_bota %>% 
+  group_by(annee,bande, seme) %>% 
+  tally()
+
+Bande_Shanon = Releve_bota %>% 
+  group_by(annee,bande) %>% 
+  mutate(Indi_Shannon = - sum((recouvrement/sum(recouvrement))*log(recouvrement/sum(recouvrement)))) %>% 
+  select(bande,annee,Indi_Shannon) %>% 
+  distinct()
+
+# Ecriture ----------------------------------------------------------------
+
+saveRDS(Releve_bota, file.path(FolderDonnees, FolderInter, "Releve_bota.rds"))
+saveRDS(Richesse_spe, file.path(FolderDonnees, FolderInter, "Richesse_spe_bande.rds"))

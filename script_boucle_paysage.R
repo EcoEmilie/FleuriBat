a175e56d1cf67865c889259e0089726546d827e1
Ecoemilie <pen.emilie@hotmail.fr>
2023-06-01 12:12:46 +0200

    Update script_boucle_paysage.R
    Modification du calcule de pourcentage et de la surface BIO
    Le bio est la proportion de bio dans la surface agricole .
    calcul de la surface en fusionnant les polygones pour éviter le chevauchement

--- script_boucle_paysage.R
+++ script_boucle_paysage.R
@@ -37,7 +37,8 @@ data_site= readRDS(file = file.path(FolderRDS, "data_site.rds")) %>% #A changer 
   unite(Mod_pass, Modalite_protocole, Num_passag, sep = "_", remove = FALSE)%>% 
   #slice_sample(n = 10) %>% 
   st_as_sf(coords = c("X","Y"), crs = 4326) %>% #au départ en WGS 84
-  st_transform(2154) 
+  st_transform(2154) %>% 
+  filter(!Modalite_protocole == "exclos")
 
 ## Carte -------------------------------------------------------------------
 
@@ -51,7 +52,9 @@ dist_habitation = c()
 ### Forêt  -------------------------------------------------------------
 
 data_foret = st_read(dsn = file.path(Folderpath,FolderCarto,"data_occupation_foret_total.gpkg")) %>% 
-  st_transform(2154)
+  st_transform(2154)  
+  
+
 dist_foret = c() 
 
 # #Feuillu
@@ -99,24 +102,24 @@ data_route = st_read(dsn = file.path(Folderpath,FolderCarto,"data_route_total.gp
 
 #2019 
 RPG_2019 = readRDS(file.path(FolderRDS,"donnees_RPG_2019.rds")) %>% 
-  st_transform(2154)
+  st_transform(2154) 
 
 RPG_cultu_2019 = RPG_2019 %>% 
-  filter(!CODE_CULTU == c("11","17","18", "28"))
+  filter(!CODE_CULTU %in% c("11","17","18", "28"))
 
 #2020 
 RPG_2020 = readRDS(file.path(FolderRDS,"donnees_RPG_2020.rds"))%>% 
   st_transform(2154)
 
 RPG_cultu_2020 = RPG_2020%>% 
-  filter(!CODE_CULTU == c("11","17","18", "28"))
+  filter(!CODE_CULTU%in%c("11","17","18", "28"))
 
 #2021
 RPG_2021 = readRDS(file.path(FolderRDS,"donnees_RPG_2021.rds"))%>% 
   st_transform(2154)
 
 RPG_cultu_2021 = RPG_2021%>% 
-  filter(!CODE_CULTU == c("11","17","18", "28"))
+  filter(!CODE_CULTU%in%c("11","17","18", "28"))
 
 nb_parcelle= c()
 
@@ -135,28 +138,28 @@ i = data_frame()
 ### Prairie ----
 #2019 
 prairie_2019 = RPG_2019 %>% 
-  filter(CODE_CULTU == c("PPH","PRL","PTR"))
+  filter(CODE_CULTU%in%c("PPH","PRL","PTR"))
 
 #2020 
 prairie_2020 = RPG_2020 %>% 
-  filter(CODE_CULTU == c("PPH","PRL","PTR"))
+  filter(CODE_CULTU%in%c("PPH","PRL","PTR"))
 
 #2021 
 prairie_2021 = RPG_2021 %>% 
-  filter(CODE_CULTU == c("PPH","PRL","PTR"))
+  filter(CODE_CULTU%in%c("PPH","PRL","PTR"))
 
 #prairie permanente
 #2019 
 praiperm_2019 = RPG_2019 %>% 
-  filter(CODE_CULTU == c("PPH","PRL"))
+  filter(CODE_CULTU%in%c("PPH","PRL"))
 
 #2020 
 praiperm_2020 = RPG_2020 %>% 
-  filter(CODE_CULTU == c("PPH","PRL"))
+  filter(CODE_CULTU%in%c("PPH","PRL"))
 
 #2021 
 praiperm_2021 = RPG_2021 %>% 
-  filter(CODE_CULTU == c("PPH","PRL"))
+  filter(CODE_CULTU%in%c("PPH","PRL"))
 
 #prairie temporaire
 #2019 
@@ -192,7 +195,6 @@ BIO_2021 = st_read(dsn = file.path(Folderpath,FolderCarto,"donnees_BIO_2021.gpkg
 
 data_naturel = rast(file.path(Folderpath,FolderCarto,"RASTERS/data_naturel.tif"))
 
-
 # Calcule des variables paysagères  ---------------------------------------
 
 #Départ
@@ -201,25 +203,24 @@ buffer = c(100,500,1000,2000)
 
 
 
-
 #selection de la ligne 
 for (i in 1:nrow(data_site)){
   names_year = data_site[i,c("carre_year_pass","year","Modalite_protocole", "geometry")]
   
   ##Distance aux zones urbaines ----
-  dist_habitation = st_distance(names_year, data_habitation, by_element = FALSE) %>% 
+  dist_habitation = st_distance(names_year, data_habitation, by_element = FALSE) %>%
     min()
-  
+
   ##Distance à la forêt ----
-  dist_foret = st_distance(names_year, data_foret, by_element = FALSE) %>% 
+  dist_foret = st_distance(names_year, data_foret, by_element = FALSE) %>%
     min()
-  
+
   ##Distance à l'eau ----
-  dist_eau = st_distance(names_year, data_eau, by_element = FALSE) %>% 
+  dist_eau = st_distance(names_year, data_eau, by_element = FALSE) %>%
     min()
-  
+
   ##Distance à la haie ----
-  dist_haie = st_distance(names_year, data_haie, by_element = FALSE) %>% 
+  dist_haie = st_distance(names_year, data_haie, by_element = FALSE) %>%
     min()
   
   for (j in 1:length(buffer)){
@@ -230,108 +231,93 @@ for (i in 1:nrow(data_site)){
     b_v = vect(b)
     buffer_area = st_area(b)
     
-    ##Surace de forêt total----
-    a = st_intersection(data_foret , b)%>% 
-      mutate(area = (st_area(geom)* 100)/buffer_area) %>% 
-      distinct() 
-    area_foret = c(ifelse(nrow(a) == 0, 0, sum(a$area)))
-    
+    ##Surface de forêt total----
+    a = st_intersection(data_foret , b) 
+    area_foret = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/buffer_area))
+
     ##Surface zone urbaine ----
-    a = st_intersection(data_habitation , b) %>% 
-      mutate(area = (st_area(geom)* 100)/buffer_area)%>% 
-      distinct() 
-    area_habitation = c(ifelse(nrow(a) == 0, 0, sum(a$area)))
+    a = st_intersection(data_habitation , b) 
+    area_habitation = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/buffer_area))
     
+    ## Surface culture ----
+    c = if(b$year == 2019){st_intersection(RPG_cultu_2019 , b)}else if(b$year==2020){st_intersection(RPG_cultu_2020 , b)} else{st_intersection(RPG_cultu_2021 , b)}
+    # a = a  %>%
+    #   mutate(area = (st_area(st_union(geometry))* 100)/buffer_area)
+    
+    area_agri = c(ifelse(nrow(c) == 0, 0,  st_area(st_union(c$geom))* 100/buffer_area))
+
     ##Surface de BIO----
-    
-    a = if(b$year == 2019){st_intersection(BIO_2019 , b)}else if(b$year==2020){st_intersection(BIO_2020 , b)} else{st_intersection(BIO_2021 , b)}
-    a = a  %>% 
-      mutate(area = (st_area(geom)* 100)/buffer_area) %>% 
-      distinct()
+
+      a = if(b$year == 2019){st_intersection(BIO_2019 , b)}else if(b$year==2020){st_intersection(BIO_2020 , b)} else{st_intersection(BIO_2021 , b)}
       
-    area_BIO = c(ifelse(nrow(a) == 0, 0,sum(a$area)))
+      area_BIO = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/st_area(st_union(c$geom))))
 
     ##Diversité de culture ----
-  
-    a = if(b$year == 2019){mask(crop(Div_2019, b_v),b_v)}else if(b$year==2020){mask(crop(Div_2020, b_v),b_v)} else{mask(crop(Div_2021, b_v),b_v)} 
-    Shannon_cultu = lsm_l_shdi(a)
-    
+
+      a = if(b$year == 2019){mask(crop(Div_2019, b_v),b_v)}else if(b$year==2020){mask(crop(Div_2020, b_v),b_v)} else{mask(crop(Div_2021, b_v),b_v)}
+      Shannon_cultu = lsm_l_shdi(a)
+
     ## Surface/moyenne/perimetre agricole ----
-    
+
     a = if(b$year == 2019){st_intersects(RPG_2019 , b)}else if(b$year==2020){st_intersects(RPG_2020 , b)} else{st_intersects(RPG_2021 , b)}
-    a <- (a %>% as.data.frame())$row.id 
+    a <- (a %>% as.data.frame())$row.id
     if(b$year == 2019){site_int <- RPG_2019[a,]}else if(b$year==2020){site_int <- RPG_2020[a,]} else{site_int <- RPG_2021[a,]}
-    
-    site_int = site_int  %>% 
-      mutate(area = st_area(geometry), perimeter = st_perimeter(geometry))%>% 
-      distinct() %>% 
+
+    site_int = site_int  %>%
+      mutate(area = st_area(geometry), perimeter = st_perimeter(geometry))%>%
+      distinct() %>%
       add_tally(name = "n")
-    
+
     moy_area_agri = c(ifelse(nrow(site_int) == 0, 0, mean(site_int$area / 10000)))
     perimeter_agri = c(ifelse(nrow(site_int) == 0, 0, mean(site_int$perimeter)))
     nb_parcelle =  c(ifelse(nrow(site_int) == 0, 0, site_int$n[1]))
-    
-    ## Surface culture ----
-    a = if(b$year == 2019){st_intersection(RPG_cultu_2019 , b)}else if(b$year==2020){st_intersection(RPG_cultu_2020 , b)} else{st_intersection(RPG_cultu_2021 , b)}
-    a = a  %>% 
-      mutate(area = st_area(geometry))%>% 
-      distinct() 
-    
-    area_agri = c(ifelse(nrow(a) == 0, 0, (sum(a$area) * 100)/buffer_area))
-    
+
     ## Surface de prairie total ----
-    
+
     a = if(b$year == 2019){st_intersection(prairie_2019 , b)}else if(b$year==2020){st_intersection(prairie_2020 , b)} else{st_intersection(prairie_2021 , b)}
-    a = a  %>% 
-      mutate(area = (st_area(geometry)* 100)/buffer_area)%>% 
-      distinct()
-      area_prairie = c(ifelse(nrow(a) == 0, 0, sum(a$area)))
-    
+    area_prairie = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/buffer_area))
+
     ## Surface de prairie permanente ----
-    
+
     a = if(b$year == 2019){st_intersection(praiperm_2019 , b)}else if(b$year==2020){st_intersection(praiperm_2020 , b)} else{st_intersection(praiperm_2021 , b)}
-    a = a  %>% 
-      mutate(area = (st_area(geometry)* 100)/buffer_area)%>% 
-      distinct()
-      area_praiperm = c(ifelse(nrow(a) == 0, 0, sum(a$area)))
-    
+   
+      area_praiperm = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/buffer_area))
+
     ## Surface de prairie temporaire ----
-    
+
     a = if(b$year == 2019){st_intersection(praitemp_2019 , b)}else if(b$year==2020){st_intersection(praitemp_2020 , b)} else{st_intersection(praitemp_2021 , b)}
-    a = a  %>% 
-      mutate(area = (st_area(geometry)* 100)/buffer_area)%>% 
-      distinct()
-    area_praitemp = c(ifelse(nrow(a) == 0, 0, sum(a$area)))
-    
-                      
+
+    area_praitemp = c(ifelse(nrow(a) == 0, 0,  st_area(st_union(a$geom))* 100/buffer_area))
+
+
     ## Densité de haie ----
     a = st_intersection(data_haie, b) %>%
       mutate(longueur = st_length(geom))
     haie_density = c(ifelse(nrow(a) == 0, 0,(sum(a$longueur)/buffer_area) * 10000))
-    
+
     ##Densité de route----
     a = st_intersection(data_route, b) %>%
       mutate(longueur = st_length(geom))
     route_density = c(ifelse(nrow(a) == 0, 0,(sum(a$longueur)/buffer_area) * 10000))
-    
-    ##Diversité d'élément semi-naturel/naturel ----
-    a = mask(crop(data_naturel, b_v),b_v)
-    Shannon_naturel= lsm_l_shdi(a)
+
+    # ##Diversité d'élément semi-naturel/naturel ----
+    # a = mask(crop(data_naturel, b_v),b_v)
+    # Shannon_naturel= lsm_l_shdi(a)
 
 
     ##Densité de lisière ----
     # a = st_intersection(data_lisiere, b) %>%
     #   mutate(longueur = st_length(geom))
     # lisiere_density = c(ifelse(nrow(a) == 0, 0,(sum(a$longueur)/buffer_area) * 10000))
-    # 
 
-    
+
+
     ## !!! collage dans le vecteur ----
     vecteur_var = cbind(b,
-                        dist_buffer, 
+                        dist_buffer,
                         buffer_area,
-                        dist_eau, 
-                        dist_habitation, 
+                        dist_eau,
+                        dist_habitation,
                         dist_foret,
                         dist_haie,
                         area_foret,
@@ -347,8 +333,9 @@ for (i in 1:nrow(data_site)){
                         route_density,
                         #lisiere_density,
                         nb_parcelle,
-                        Shannon_naturel = Shannon_naturel$value,
-                        Shannon_cultu = Shannon_cultu$value)
+                        #Shannon_naturel = Shannon_naturel$value,
+                        Shannon_cultu = Shannon_cultu$value
+                        )
     
     ## !!! collage ligne par ligne----
     data_pay = rbind(data_pay,vecteur_var)
@@ -363,5 +350,43 @@ FolderInter= "2.Donnees_intermediaire"
 saveRDS(data_pay,file.path(FolderDonnees,FolderInter,"data_paysage.rds"))
 st_write(data_pay, file.path(FolderDonnees,FolderInter,"data_paysage.gpkg"), driver = "GPKG", append = FALSE)
 st_write(data_site, file.path(FolderDonnees,FolderInter,"data_site.gpkg"), driver = "GPKG", append = FALSE)
- 
+
+data_probleme = data_pay %>% 
+  filter(area_agri > 100.01) %>% 
+  group_by(carre_year_pass) %>% 
+  tally() %>% 
+  filter(n == 4)
+
+data_BIO_probleme = data_pay %>% 
+  filter(area_BIO > 100.01) 
+
+data_site_pro = data_probleme %>% 
+  select(carre_year_pass) %>% 
+  distinct() 
+
+data_site_pro2 = data_site %>% 
+  filter(carre_year_pass %in% data_site_pro$carre_year_pass )
+
+data_agri = data_pay
+
+data_pay_agri = data_pay %>% 
+  select(carre_year_pass, year, Modalite_protocole, dist_buffer, area_agri) %>% 
+  filter(!carre_year_pass %in% data_pay2$carre_year_pass) %>% 
+  bind_rows(data_pay2) %>% 
+  st_drop_geometry()
+  
+data_pay_modif = data_pay %>% 
+  filter(area_agri < 100.01)%>% 
+  filter(area_BIO < 100.01) 
+
+saveRDS(data_pay,file.path(FolderDonnees,FolderInter,"data_paysage.rds"))
+st_write(data_pay, file.path(FolderDonnees,FolderInter,"data_paysage.gpkg"), driver = "GPKG", append = FALSE)
+
+data_pay = readRDS(file.path(FolderDonnees,FolderInter,"data_paysage.rds"))
+
+saveRDS(data_pay_modif,file.path(FolderDonnees,FolderInter,"data_paysage_modif.rds"))
+st_write(data_pay_modif, file.path(FolderDonnees,FolderInter,"data_paysage_modif.gpkg"), driver = "GPKG", append = FALSE)
+
+
+
 
